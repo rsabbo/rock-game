e83ae31dd50e28bc9dc6bd4be1ea37680d31cd91
“Rita” <23293771+rsabbo@users.noreply.github.co>
2023-01-21 22:24:09 +0100

    final game: updated playerChoice for null and incorrect answers and added the answers to the messages displayed on the round game

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
-//function declaration with computer choices and return for a random choice
+//possible choices for the players
+const choices = ["Rock", "Paper", "Scissors"];
+
+//function declaration that returns a random choice from the choices array
 function computerChoice() {
-  const choices = ["Rock", "Paper", "Scissors"];
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
 //player choice function declaration
 function playerChoice() {
-  let playerInput = prompt("Rock, Paper or Scissors?");
-  //makes input first letter uppercase and the rest lowercase
-  playerInput =
-    playerInput.charAt(0).toUpperCase() + playerInput.slice(1).toLowerCase();
+  let playerInput = prompt(
+    "Which one will you play?",
+    "Rock, Paper or Scissors"
+  );
 
-  return playerInput;
+  //check if the player input is null and if not transform the written word in a word with the first letter uppercase and the rest lowercase
+  let input =
+    playerInput === null
+      ? "This is not a valid answer"
+      : playerInput.charAt(0).toUpperCase() +
+        playerInput.slice(1).toLowerCase();
+
+  //if the written word by the user it's not the same as the words on the choices array ["Rock", "Paper", "Scissors"] return "not valid answer" otherwise if it matches use the answer
+  if (choices.indexOf(input) === -1) {
+    return "Not a valid answer!";
+  } else {
+    return input;
+  }
 }
 
 //game results
@@ -21,17 +35,17 @@ let compScore = 0;
 //function declaration with playRound and adding score to the round's winner
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    return "It's a tie!";
+    return `It's a tie! You both picked ${playerSelection}`;
   } else if (
     (playerSelection === "Rock" && computerSelection === "Scissors") ||
     (playerSelection === "Scissors" && computerSelection === "Paper") ||
     (playerSelection === "Paper" && computerSelection === "Rock")
   ) {
     playerScore++;
-    return "Player wins this round!";
+    return `Player wins this round because ${playerSelection} beats ${computerSelection}!`;
   } else {
     compScore++;
-    return "Computer wins this round!";
+    return `Computer wins this round because ${playerSelection} beats ${playerSelection}!`;
   }
 }
 
